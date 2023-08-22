a1d3e3492faea48a00749664c07e8b7e30bb26e0
OlehOreo <128093567+OlehOreo@users.noreply.github.com>
2023-08-22 08:33:41 +0000

    Deploying to gh-pages from @ OlehOreo/goit-js-hw-09@424565507a070752891e6083c31dfdc8ef6d39ae 🚀

---
+++ 01-color-switcher.96eb4902.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");document.querySelectorAll("button");t.addEventListener("click",(function(){colorId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),e.removeAttribute("disabled"),t.setAttribute("disabled",!0)}),1e3)})),e.addEventListener("click",(function(){clearInterval(colorId),t.removeAttribute("disabled"),e.setAttribute("disabled",!1)}))}();
+//# sourceMappingURL=01-color-switcher.96eb4902.js.map
